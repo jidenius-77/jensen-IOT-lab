c51e2446e8f769e5d40682d8a421c7b028da26f9
Jidenius <jidenius@live.se>
2026-08-24 14:49:55 +0200

    todo m2 redis läsa cashe

--- api/cache.py
+++ api/cache.py
@@ -12,10 +12,15 @@ client = redis.Redis(
 def get_latest_from_cache(device_id):
     # TODO M2:
     # Läs senaste mätvärdet från Redis.
+    data = client.get(f"latest:{device_id}")
+    if data:
+        return json.loads(data)
+
     return None
 
 
 def set_latest_in_cache(device_id, measurement):
     # TODO M2:
     # Spara senaste mätvärdet i Redis.
-    pass
+    if measurement is not None:
+        client.set(f"latest:{device_id}", json.dumps(measurement))
